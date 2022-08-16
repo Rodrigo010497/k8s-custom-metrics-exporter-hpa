fe9c848389525ca6100defc178bb69c198274582
psu <psu@jwplayer.com>
2022-08-16 17:19:19 +0200

    fix some bug, can increment and decrement now

--- python-custom-autoscaler/metric.py
+++ python-custom-autoscaler/metric.py
@@ -1,3 +1,5 @@
+
+
 # Copyright 2020 The Custom Pod Autoscaler Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,16 +16,10 @@
 
 import json
 import sys
-import math
+import requests
 
 # JSON piped into this script example:
 # {
-#   "metrics": [
-#     {
-#       "resource": "flask-metric-869879868f-jgbg4",
-#       "value": "{\"value\": 0, \"available\": 5, \"min\": 0, \"max\": 5}"
-#     }
-#   ],
 #   "resource": {
 #     "kind": "Deployment",
 #     "apiVersion": "apps/v1",
@@ -33,39 +29,32 @@ import math
 #     },
 #     ...
 #   },
-#   "runType": "api"
+#   "runType": "scaler"
 # }
 
 def main():
     # Parse JSON into a dict
     spec = json.loads(sys.stdin.read())
-    evaluate(spec)
+    metric(spec)
 
-def evaluate(spec):
-    # Count total available
-    total_available = 0
-    for metric in spec["metrics"]:
-        json_value = json.loads(metric["value"])
-        available = json_value["available"]
-        total_available += int(available)
-
-    # Get current replica count
-    target_replica_count = int(spec["resource"]["spec"]["replicas"])
-
-    # Decrease target replicas if more than 5 available
-    if total_available > 5:
-        target_replica_count -= 1
-
-    # Increase target replicas if none available
-    if total_available <= 0:
-        target_replica_count += 1
-
-    # Build JSON dict with targetReplicas
-    evaluation = {}
-    evaluation["targetReplicas"] = target_replica_count
-
-    # Output JSON to stdout
-    sys.stdout.write(json.dumps(evaluation))
+def metric(spec):
+    # Get Pod IP
+    status = spec["resource"]["status"]
+    ip = status["podIP"]
+    try:
+        # Make request to Pod metric endpoint
+        # (see ../flask-metrics/ folder for simple flask app exposing this endpoint)
+        response = requests.get(f"http://{ip}:5000/metric")
+        # Output whatever metrics are gathered to stdout
+        sys.stdout.write(response.text)
+    except HTTPError as http_err:
+        # If an error occurs, output error to stderr and exit with status 1
+        sys.stderr.write(f"HTTP error occurred: {http_err}")
+        exit(1)
+    except Exception as err:
+        # If an error occurs, output error to stderr and exit with status 1
+        sys.stderr.write(f"Other error occurred: {err}")
+        exit(1)
 
 if __name__ == "__main__":
     main()
